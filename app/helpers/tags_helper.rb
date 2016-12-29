1b539a3e40acbf724d3370ba2af007a02291c01c
gilcierweb <gilcier06@yahoo.com.br>
2016-12-29 15:51:38 -0300

    add dawnload image

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,5 +1,5 @@
 module TagsHelper
   def clear_tags(tag)
-    tag.split(',').join(",<br> ").gsub(/^\["+|\"]+$/, "").gsub('"', '').gsub('\n', '').html_safe
+    tag.split(',').join(",<br> ").gsub(/^\["+|\"]+$/, "").gsub('"', '').gsub('\n', '').gsub('\t', '').html_safe
   end
 end
--- lib/crawler/crawler_tags.rb
+++ lib/crawler/crawler_tags.rb
@@ -8,12 +8,20 @@ class CrawlerTags
     @url = url
   end
 
+  def strip_url(url)
+    url.gsub("http://", "").gsub("https://", "").gsub("www.", "").gsub("/", "").gsub(".", "-")
+  end
+
   def crawler_data
 
     tags = nil
 
     html = open(@url)
     document = Nokogiri::HTML(html)
+
+    url_folder = strip_url(@url)
+    dir = "#{Rails.root}/public/sites/#{url_folder}/"
+    Dir.mkdir(dir) unless File.directory?(dir)
 
     document.css('body').each do |tag|
 
@@ -24,6 +32,12 @@ class CrawlerTags
       h5 = tag.search('h5').map(&:text)
       h6 = tag.search('h6').map(&:text)
       link = tag.search('a').map { |link| link['href'] }
+
+      document.css("img").each do |item|
+        if item['src'] =~ /([^\/]+)$/
+          File.open(File.join(dir, $1), 'wb') { |f| f.write(open(item['src']).read) }
+        end
+      end
 
       return tags = {
           h1: h1.to_s,
